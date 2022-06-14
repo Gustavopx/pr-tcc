6df5b2c42b350e8c689745905be438ab7d5ad72f
Gustavo Montanher <gustavomontanher06@gmail.com>
2022-06-14 11:16:14 -0300

    90% do html e css feito

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import App from './pages/App.js';
+import Home from './pages/home.js';
+import Login from './login.js';
+import Menu from './menu.js';
+import Menu2 from './menu2.js';
+import Menu3 from './menu3.js';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path='/home' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/menu' element={<Menu />} />
+      <Route path='/menu2' element={<Menu2 />} />
+      <Route path='/menu3' element={<Menu3 />} />
+      <Route path='/app' element={<App />} />
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
